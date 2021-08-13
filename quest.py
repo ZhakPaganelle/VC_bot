ebb091dda2e7383d5f35aee50e8eb7ca2cf2d554
ZhakPaganelle <daniilprozorkevich@mail.ru>
2021-08-13 14:34:28 +0300

    Changed work mechanism with users db

---
+++ quest.py
@@ -0,0 +1,8 @@
+class QuestStage:
+    def __init__(self, step_id, text, score, answer=None):
+        self.step_id = step_id
+        self.text = text
+        self.score = score
+        self.answer = answer
+
+quest_list = []
